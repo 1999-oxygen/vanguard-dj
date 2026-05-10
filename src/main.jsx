e77348c501f37d47976bea9c93342e9b8bc90938
Nimrod <81225270+1999-oxygen@users.noreply.github.com>
2026-05-10 06:38:50 +0300

    Add src/main.jsx

---
+++ src/main.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import VanguardDJ from './VanguardDJ.jsx'
+import './index.css'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <VanguardDJ />
+  </React.StrictMode>,
+)
